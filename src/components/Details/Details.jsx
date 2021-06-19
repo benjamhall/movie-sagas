c820e6bf13aee4e08963a5d59afb2bb3e7e6168b
Ben Hall <benjamhall@gmail.com>
2021-06-19 10:24:15 -0500

    genres are displayed in details

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 import Button from '@material-ui/core/Button';
+import {useHistory} from "react-router-dom";
 
 
 function Details() {
+    const history = useHistory();
     
     const details = useSelector(store => store.details)
     const genres = useSelector(store => store.genres)
@@ -20,16 +22,18 @@ function Details() {
             <h1>{details.title}</h1>
             <img src={details.poster}></img>
             <p>{details.description}</p>
-            <Button variant="outlined" color="primary" onClick={handleBack}>Back to List</Button>
+            
             <h2>Genres:</h2>
             <ul>
-                {genres.map((genre, i) => {
+                {genres.map((genre) => {
                 return (
-                <li>{genre.name}</li>
-                )
-            })
-                
+                <li key={genre.id}>
+                    {genre.name}
+                </li>
+                );
+            })}
             </ul>
+            <Button variant="outlined" color="primary" onClick={handleBack}>Back to List</Button>
         </>
     )
 }
