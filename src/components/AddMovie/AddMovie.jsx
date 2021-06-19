25eb3b88b59de3361e364f1ba77d702291f15dc7
Ben Hall <benjamhall@gmail.com>
2021-06-19 11:05:47 -0500

    setup TextField with MenuItems in Add Movie

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
-function AddMovie () {
+function AddMovie() {
 
 
     return (
         <div>
-            
+            <h1>Add Movie</h1>
+            <form>
+                <TextField id="outlined-basic" label="Movie Title" variant="outlined" />
+                <TextField id="outlined-basic" label="Image URL" variant="outlined" />
+                <TextField id="outlined-basic" label="Movie Description" variant="outlined" />
+                <Select defaultValue="" id="demo-simple-select">
+                    <MenuItem value={"Adventure"}>Adventure</MenuItem>
+                    <MenuItem value={"Animated"}>Animated</MenuItem>
+                    <MenuItem value={"Biographical"}>Biographical</MenuItem>
+                    <MenuItem value={"Comedy"}>Comedy</MenuItem>
+                    <MenuItem value={"Disaster"}>Diaster</MenuItem>
+                    <MenuItem value={"Drama"}>Drama</MenuItem>
+            </Select>
+            </form>
+
+
         </div>
-    )
+            )
 
 }
 
-export default AddMovie;
+            export default AddMovie;
